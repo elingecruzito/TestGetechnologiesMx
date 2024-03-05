1f0090d0e5e52999fb3e6e2bfcc0638fb116381d
andres.garcia <andres.garcia@banregio.com>
2024-03-05 13:37:54 -0600

    Se modifico el caso de uso de las salidas

--- src/main/java/mx/getechnologies/test/controllers/SalidasController.java
+++ src/main/java/mx/getechnologies/test/controllers/SalidasController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import mx.getechnologies.test.models.EntradasVehiculosModel;
 import mx.getechnologies.test.models.SalidasVehiculosModel;
+import mx.getechnologies.test.models.TiposVehiculo;
 import mx.getechnologies.test.repositories.EntradasRepository;
 import mx.getechnologies.test.repositories.SalidasRepository;
 
@@ -28,14 +29,50 @@ public class SalidasController {
 
 
     private EntradasVehiculosModel entrada;
+    private TiposVehiculo tipoVehiculo;
+    private String menssage;
 
     @PostMapping("/nueva")
-    public SalidasVehiculosModel nuevaSalida(@RequestBody SalidasVehiculosModel model) {
+    public String nuevaSalida(@RequestBody SalidasVehiculosModel model) {
         entrada = entradasRepository.getLastEntrada(model.getPlaca());
         long milisegundos = System.currentTimeMillis() - entrada.getEntrada().getTime();
         int minutos = (int) ((milisegundos / (1000 * 60)) % 60);
-        //model.setImporteTotal((placa.getImporte() * minutos));
-        return repository.save(model);
+        model.setImporteTotal((getImporet(entrada.getTipo()) * minutos));
+        model = repository.save(model);
+        switch (tipoVehiculo) {
+            case OFICIAL:
+                menssage = "Se registro con exito su salida, Buen viaje!";
+                break;
+
+            case RESIDENTE:
+                menssage = "El importe de $" + model.getImporteTotal() + " se a acumulado a su cuenta, Buen viaje!";
+            break;
+                
+            default:
+                menssage = "Su importe es de $" + model.getImporteTotal();
+                break;
+        }
+        return menssage;
+    }
+
+    private Double getImporet(int tipo){
+        tipoVehiculo = TiposVehiculo.values()[tipo];
+        Double importe = 0.0;
+        switch (tipoVehiculo) {
+            case OFICIAL:
+                importe = 0.0;
+                break;
+
+            case RESIDENTE:
+                importe = 0.05;
+            break;
+        
+            default:
+                importe =  0.05;
+                break;
+        }
+
+        return importe;
     }
     
 
